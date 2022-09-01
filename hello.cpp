e9b43fa7e992de2daf6e4d7fc0eb64201a2a2df4
nissavanidonk <111962970+nissavanidonk@users.noreply.github.com>
2022-09-01 07:14:00 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Hello, Dcoder!";
+    cout << "nama:Bella" << endl;
+    cout << "kelas:X PPLG 1" << endl;
+    cout << "==========" << endl;
+    cout << "angka1=10" << endl;
+    cout << "angka2=5" << endl;
+    cout << "hasil:=15" << endl;
+    return 0;
+    }
